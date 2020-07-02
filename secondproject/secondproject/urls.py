56f6b09c1de0922e90c45f9a4f6ab74ea92c8861
lim <tbnsok40@naver.com>
2020-07-02 13:55:59 +0900

    blog finished

--- secondproject/secondproject/urls.py
+++ secondproject/secondproject/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='home'),
     path('blog/int:<blog_id>', blog.views.detail, name='detail'),
+    path('blog/new', blog.views.new, name='new'),
+    path('blog/create', blog.views.create, name='create'),    
 ]
 
