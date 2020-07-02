51dcfc87710ccfc1dcfe440f9f371069bffb752e
lim <tbnsok40@naver.com>
2020-07-02 13:10:50 +0900

    detail page 생성

--- secondproject/secondproject/urls.py
+++ secondproject/secondproject/urls.py
@@ -19,5 +19,6 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='home'),
+    path('blog/int:<blog_id>', blog.views.detail, name='detail'),
 ]
 
